3634639e34ac5d2c9984a662b8b3221e937cb8e9
Mendanbar <mister.yoshino@gmail.com>
2022-02-17 16:22:32 -0800

    Placeholders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles, createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { grey, blue } from '@material-ui/core/colors';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import banner from './banner.jpg'
+import bannerWide from './banner-wide.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  banner: {
+    width: '100%',
+  }
+}));
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[800],
+    },
+    secondary: blue,
+  },
+});
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
 }
 
-export default App;
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ElevateAppBar(props) {
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.root}>
+        <ElevationScroll {...props}>
+          <AppBar>
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                Free Seneca
+              </Typography>
+              <div>
+                <form action="https://www.paypal.com/donate" method="post" target="_top">
+                  <input type="hidden" name="hosted_button_id" value="BWCGG2W8CBJ46" />
+                  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                  <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+              </div>
+            </Toolbar>
+          </AppBar>
+        </ElevationScroll>
+        <Toolbar />
+        {/* <img src={banner} width="100%" className={classes.banner} alt="logo" /> */}
+        <img src={bannerWide} width="100%" className={classes.banner} alt="logo" />
+        <Container>
+          <Box my={2}>
+            {[...new Array(12)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+              )
+              .join('\n')}
+          </Box>
+        </Container>
+      </div>
+    </ThemeProvider>
+  );
+}
