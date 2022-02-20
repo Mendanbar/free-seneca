2efc7f1ae4920937670bc1c1de1330bffa949ff6
Mendanbar <mister.yoshino@gmail.com>
2022-02-20 11:28:50 -0800

    reset to simple analytics

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import Home from './Pages/Home'
 import Character from './Pages/Character'
 import Pictures from './Pages/Pictures';
 
-import GA from './utils/GoogleAnalytics'
 
 const theme = createTheme({
   palette: {
@@ -33,7 +32,6 @@ ReactDOM.render(
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        {GA.init() && <GA.RouteTracker />}
         <Header />
         <Routes>
           <Route path="/character" element={<Character />} />
